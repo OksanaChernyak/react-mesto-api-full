36dd89f29df01899f620508977d8c678006b546f
OksanaChe <oksanaadams@yandex.ru>
2022-08-19 12:41:24 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use(requestLogger);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
