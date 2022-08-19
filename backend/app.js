09fd77bd3f5c010651eaa975e01cf3f2c6a5a423
OksanaChe <oksanaadams@yandex.ru>
2022-08-19 11:47:18 +0300

    logger

--- backend/app.js
+++ backend/app.js
@@ -7,17 +7,19 @@ const cardRoute = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorsHandler = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./utils/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -39,6 +41,7 @@ app.use('/cards', auth, cardRoute);
 app.use('/*', () => {
   throw new NotFoundError('Страница  по этому адресу не найдена');
 });
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
 
