d3b822e239ef250605bdf1e4d0c60d27cac4c39e
OksanaChe <oksanaadams@yandex.ru>
2022-08-23 18:34:57 +0300

    token2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,8 +1,6 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'oksana-have-secrets');
+    payload = jwt.verify(token, 'oksana-have-secrets');
   } catch (err) {
     throw new UnauthorizedError('Переданы неверные данные');
   }
