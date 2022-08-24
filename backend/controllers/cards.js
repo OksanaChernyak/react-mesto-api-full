e630c9a232e07183eb507b978454082d426143b0
OksanaChe <oksanaadams@yandex.ru>
2022-08-24 09:50:23 +0300

    array

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../utils/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send([cards]))
     .catch(() => {
       next();
     });
